64ff82ab47baec3ed1786a889567a0793e50bad5
Khushi Srivastava <sweetkhushi2000@gmail.com>
2026-02-13 19:39:16 +0530

    capitalizing Pages folder

--- server/server.js
+++ server/server.js
@@ -11,15 +11,12 @@ const app = express();
 const port = process.env.PORT || 4000;
 connectDB();
 
-// I added process.env.FRONTEND_URL here.
-// You will need to add this variable to Vercel later!
 const allowedOrigins = ["http://localhost:5173", process.env.FRONTEND_URL];
 
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({ origin: allowedOrigins, credentials: true }));
 
-// api endpoints
 app.get("/", (req, res) => {
   res.send("api working...");
 });
@@ -27,10 +24,8 @@ app.get("/", (req, res) => {
 app.use("/api/auth", authRouter);
 app.use("/api/user", userRouter);
 
-// Prevent app.listen from running in Vercel's serverless production environment
 if (process.env.NODE_ENV !== "production") {
   app.listen(port, () => console.log(`server started AT port : ${port}`));
 }
 
-// THE FIX: Use ES Module export instead of CommonJS
 export default app;
